41bf1ae79355d7c472c6ff123c5f91eb450f2c30
thanhminhspk05 <thanhminhspk05@gmail.com>
2023-06-19 16:41:19 +0700

    lesson 173

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+// import './index.css';
+import App2 from './useGeolocation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App2 />
   </React.StrictMode>
 );
 
